b685550bcc2058acdf4541a432b3174a3d8de970
hyang23 <hyang23@ncsu.edu>
2021-06-01 20:48:46 -0400

    MultiVariate Analysis

--- PA/Code/MultiVariate/multi1.R
+++ PA/Code/MultiVariate/multi1.R
@@ -3,50 +3,27 @@ library(dplyr)
 library(maps)
 library(spBayes)
 library(viridis) 
-
+rm(list=ls())
 setwd("/Users/hongjianyang/Research/PAStudy/PA/")
 source('Code/Tools/myFunc.R')
-pa2020 <- read.csv('Data/NC_PA_FRM_PM/PA_2020_hourly_PM_NC.csv')
-epa2020 <- read.csv('Data/NC_PA_FRM_PM/FRM_2020_hourly_PM_NC.csv')
 
+
+## This is also a two-stage model
+# In stage I, kriging on Purple Air only
+# Get prediction readings in both epa_train
+# In stage II, do a multivariate analysis and predict at epa_test
+
+pa <- read.csv('Data/Formatted_PA_FRM/PA_2020_hourly_Formatted.csv')
+epa <- read.csv('Data/Formatted_PA_FRM/FRM_2020_hourly_Formatted.csv')
+pa$Timestamp <- as.POSIXct(pa$Timestamp)
+epa$Timestamp <- as.POSIXct(epa$Timestamp)
 ## Global variables
 # 80% of EPA stations were used during training
 trainPercent = 0.8
-# Number of covariates in stage 1: Lon, Lat, lon^2, lat^2, lon*lat, intercept
+# Number of covariates in prediction: Lon, Lat, lon^2, lat^2, lon*lat, intercept
 p1 = 6
-# Number of covariates in stage 2: Lon, Lat, lon^2, lat^2, lon*lat, X(s), intercept
-p2 = 7
-
-col <- c('Lon', 'Lat', 'Timestamp', 'PM25_corrected')
-pa <- pa2020 %>%
-  select(col) %>%
-  rename(PM25 = PM25_corrected) %>%
-  group_by(Lon, Lat, Timestamp) %>%
-  summarise(PM25 = mean(PM25))
-
-pa <- pa[order(pa$Lon, pa$Lat, pa$Timestamp), ]
-# Remove NAs
-pa <- pa[complete.cases(pa), ]
-pa$Timestamp <- as.POSIXct(pa$Timestamp)
-
-# Process EPA data
-epa2020$Timestamp <- paste(epa2020$Date_GMT, epa2020$Hour_GMT)
-epa2020$Timestamp <- paste(epa2020$Timestamp, ':00', sep = '')
-col <- c('Lon', 'Lat', 'Timestamp', 'PM25')
-epa <- epa2020 %>% 
-  select(col)
-epa <- epa[order(epa$Lon, epa$Lat, epa$Timestamp), ]
-epa$Timestamp <- as.POSIXct(epa$Timestamp, format = '%Y-%m-%d %H:%M:%OS')
-
-write.csv(pa, 'Data/Formatted_PA_FRM/PA_2020_Hourly_Formatted.csv', row.names = FALSE)
-write.csv(epa, 'Data/Formatted_PA_FRM/FRM_2020_Hourly_Formatted.csv', row.names = FALSE)
-
-
-
-
 
 # Get 05/01/2020 - 05/01/2020 data
-
 start <- as.POSIXct('2020-05-01 00:00:00')
 end <- as.POSIXct('2020-05-01 00:00:00')
 pa <- subset(pa, Timestamp >= start & Timestamp <= end)
@@ -75,46 +52,65 @@ n_timestamp <- diff / 3600 + 1
 n.samples <- 25000
 burn      <- 5000
 
+epa_train_sub <- epa_train
+epa_test_sub <- epa_test
+pa_sub <- pa
 
-  epa_train_sub <- epa_train
-  epa_test_sub <- epa_test
-  pa_sub <- pa
 
-
-    # Stage 1: Kriging on Purple Air data
-    S <- data.matrix(pa_sub[, 1:2])
-    maxd      <- max(dist(S))
-    # Construct locations
-    X <- S
-    X <- dfColNorm(X)
-    X <- locOpe(X[, 1], X[, 2])
+### Stage I: prediction with Purple Air data only
+S <- data.matrix(pa_sub[, 1:2])
+maxd      <- max(dist(S))
+# Construct Purple Air covariates and observations
+X <- S
+X <- dfColNorm(X)
+x.1 <- locOpe(X[, 1], X[, 2])
+y.1 <- pa_sub$PM25
     
-    # Model: Y = \beta_{0} + \beta_{1}*Lon + \beta_{2}*Lat + \beta_{3}*Lon^2 + 
-    # \beta_{4}*Lat^2 + \beta_{5}*LonLat
-    Y <- pa_sub$PM25
-    starting  <- list("phi"=1/(0.05*maxd), "sigma.sq"=0.5*var(Y), "tau.sq"=0.5*var(Y))
-    tuning    <- list("phi"=1, "sigma.sq"=0.05*var(Y), "tau.sq"=0.05*var(Y))
-    amcmc     <- list("n.batch"=n.samples/100, "batch.length"=100, "accept.rate"=0.43)
-    priors    <- list("beta.Norm"=list(rep(0,p1), 100*diag(p1)),
+starting  <- list("phi"=1/(0.05*maxd), "sigma.sq"=0.5*var(Y), "tau.sq"=0.5*var(Y))
+tuning    <- list("phi"=1, "sigma.sq"=0.05*var(Y), "tau.sq"=0.05*var(Y))
+amcmc     <- list("n.batch"=n.samples/100, "batch.length"=100, "accept.rate"=0.43)
+priors    <- list("beta.Norm"=list(rep(0,p1), 100*diag(p1)),
                       "phi.Unif"=c(1/(2*maxd), 1/(0.01*maxd)),
                       "sigma.sq.IG"=c(2, 1),
                       "tau.sq.IG"=c(2, 1))
+
+
+stage1 <- spLM(y.1 ~ x.1 - 1, coords = S, starting = starting, tuning = tuning,
+                   priors = priors, cov.model = "exponential", amcmc = amcmc, n.samples=n.samples,
+                   verbose=FALSE)
+
+# Predict at both epa_train locations
+epa_train_s0 <- data.matrix(epa_train_sub[, 1:2])
+epa_train_X0 <- epa_train_s0
+epa_train_X0 <- dfColNorm(epa_train_X0)
+epa_train_X0 <- locOpe(epa_train_X0[, 1], epa_train_X0[, 2])
+
+pred_epa <- spPredict(stage1, pred.coords=epa_train_s0, pred.covars=epa_train_X0, 
+                                       start=burn, thin=10, verbose=FALSE)
+
+pred_epa_train <- pred_epa$p.y.predictive.samples
+epa_train_hat <- apply(pred_epa_train,1,mean)
+
+
+epa_test_s0 <- data.matrix(epa_test_sub[, 1:2])
+epa_test_X0 <- epa_test_s0
+epa_test_X0 <- dfColNorm(epa_test_X0)
+epa_test_X0 <- locOpe(epa_test_X0[, 1], epa_test_X0[, 2])
     
-    fit_spbayes  <- spLM(Y~X-1, coords=S,
-                         starting=starting, tuning=tuning, 
-                         priors=priors, cov.model="exponential",
-                         amcmc=amcmc, n.samples=n.samples,verbose=FALSE)
-    
+
+#### Stage II: Multivariate analysis
+# epa_train_hat: Krig
+
+    # Construct EPA covariates and observations
     epa_s0 <- data.matrix(epa_train_sub[, 1:2])
     epa_X0 <- epa_s0
-    epa_X0 <- dfColNorm(epa_X0)
-    epa_X0 <- locOpe(epa_X0[, 1], epa_X0[, 2])
+    x.2 <- dfColNorm(epa_X0)
+    x.2 <- locOpe(x.2[, 1], x.2[, 2])
+    y.2 <- epa_train_sub$PM25
     
-    # Predict at training locations
-    pred_epa <- spPredict(fit_spbayes, pred.coords=epa_s0, pred.covars=epa_X0, 
-                          start=burn, thin=10, verbose=FALSE)
-    pred_epa <- pred_epa$p.y.predictive.samples
-    epa_train_hat <- apply(pred_epa,1,mean)
+    
+    
+    
     
     # Predict at testing locations
     lon <- epa_test_sub[, 1]
@@ -127,26 +123,5 @@ burn      <- 5000
                            start=burn, thin=10, verbose=FALSE)
     pred_test <- pred_test$p.y.predictive.samples
     epa_test_hat <- apply(pred_test,1,mean)
-    
-    # Stage 2: Use Predicted values as covariates
-    # Model: E(Y) = \beta_{0} + \beta_{1} * lon + \beta_{2} * lat + \beta_{3} * lon2
-    #               + \beta_{4} * lat2 + \beta5 * lonlat + \beta6 * X(s)
-    q = 2
-    Y <- epa_train_sub$PM25
-    maxd      <- max(dist(epa_s0))
-    starting  <- list("phi"=1/(0.05*maxd), "sigma.sq"=0.5*var(Y), "tau.sq"=0.5*var(Y))
-    tuning    <- list("phi"=1, "sigma.sq"=0.05*var(Y), "tau.sq"=0.05*var(Y))
-    amcmc     <- list("n.batch"=n.samples/100, "batch.length"=100, "accept.rate"=0.43)
-    priors <- list("beta.Flat", "phi.Unif"=list(rep(3/0.75,q), rep(3/0.25,q)),
-                   "K.IW"=list(q+1, diag(0.1,q)), "Psi.ig"=list(c(2,2), c(0.1,0.1)))
-    
-    X.1 <- epa_X0
-    X.2 <- epa_X0
-    S <- epa_s0
-    fit_spbayes_stage2  <- spMvLM(list(Y~X.1 - 1, epa_train_hat~X.2 - 1), coords=S,
-                                starting=starting, tuning=tuning, 
-                                priors=priors, cov.model="exponential",
-                                amcmc=amcmc, n.samples=n.samples,verbose=FALSE)
-    
 
 
