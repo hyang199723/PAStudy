466c8c94ac88d5dd8a9c20a36fa8d6d8c591e410
sofiar <sofi.ruizsuarez@gmail.com>
2021-10-04 17:40:03 -0300

    simulate spectral process

--- PA/Code/Spectral/simAllTS.R
+++ PA/Code/Spectral/simAllTS.R
@@ -1,4 +1,4 @@
-
+source('ExtraFunctions.R')
 #### simulation of the time series process
 
 library(tidyverse)
@@ -17,26 +17,30 @@ exp_corr=function(d,range)
 
 ###### set some parameters ########
 
-n=c(600,450) # number of locations
+n=c(200,250) # number of locations
 
-TT=30 # total time steps
+nt=10 # total time steps
 
 tau1=1^2 # error variance1
 tau2=(1.5)^2  # error variance2
 set.seed(99)
-al=runif(TT)
+al=runif(nt,min=.5,max=1)
 
 # correlation parameters
 set.seed(88)
-sigmau=(1/rgamma(TT,1,1))^2
+sigmau=rnorm(nt,sd=2)+seq(from=102,to=120,by=2)
 set.seed(564)
-sigmav=(1/rgamma(TT,1,1))^2
+sigmav=rnorm(nt,sd=2)+seq(from=10,to=28,by=2)
 set.seed(213)
-rangeu=runif(TT,min=0.5,max=1.5)
+rangeu=runif(nt,min=0.5,max=1.5)
+lrangeu=log(rangeu)
 set.seed(313)
-rangev=runif(TT,min=0.5,max=1.5)
+rangev=runif(nt,min=0.5,max=1.5)
+lrangev=log(rangev)
 
-
+## mean
+beta.1=0
+beta.2=0
 ###### simulate coordinates #######
 
 set.seed(22)
@@ -48,13 +52,13 @@ coords=rbind(coords1,coords2)
 
 du12=as.matrix(dist(coords)) #distance matrix U
 
-u=matrix(NA,ncol=TT,nrow=sum(n))
-u1=matrix(NA,ncol=TT,nrow=n[1])
-u2=matrix(NA,ncol=TT,nrow=n[2])
-v2=matrix(NA,ncol=TT,nrow=n[2])
+u=matrix(NA,ncol=nt,nrow=sum(n))
+u1=matrix(NA,ncol=nt,nrow=n[1])
+u2=matrix(NA,ncol=nt,nrow=n[2])
+v2=matrix(NA,ncol=nt,nrow=n[2])
 dv2=as.matrix(dist(coords2)) # distance matrix v
 
-for (t in 1:TT)
+for (t in 1:nt)
 {
   #u
   M=exp_corr(du12,range=rangeu[t])
@@ -64,7 +68,7 @@ for (t in 1:TT)
   
   #v
   Sigmav22=exp_corr(dv2,range = rangev[t])
-  v2[,t]=t(chol(Sigmav22))%*%rnorm(n[2],0,sqrt(sigmav))
+  v2[,t]=t(chol(Sigmav22))%*%rnorm(n[2],0,sqrt(sigmav[t]))
   
 }
 
@@ -72,25 +76,32 @@ for (t in 1:TT)
 
 ####### simulate response Y ############
 
-Y1=matrix(NA,ncol=TT,nrow=n[1])
-Y2=matrix(NA,ncol=TT,nrow=n[2])
+#spectral
+Z1sp=matrix(NA,ncol=nt,nrow=n[1])
+Z2sp=matrix(NA,ncol=nt,nrow=n[2])
 
-for (t in 1:TT)
+for (t in 1:nt)
 {
-  Y1[,t]=u1[,t]+rnorm(n[1],0,sqrt(tau1))
-  Y2[,t]=al[t]*u2[,t]+v2[,t]+rnorm(n[2],0,sqrt(tau2))
+  Z1sp[,t]=u1[,t]#+rnorm(n[1],0,sqrt(nt*tau1))
+  Z2sp[,t]=al[t]*u2[,t]+v2[,t]#+rnorm(n[2],0,sqrt(nt*tau2))
   
 }
+
+#time domain
+Y1=matrix(NA,ncol=nt,nrow=n[1])
+Y2=matrix(NA,ncol=nt,nrow=n[2])
+for(i in 1:n[1]){Y1[i,] <- fft_real(Z1sp[i,],inverse=TRUE)+beta.1+rnorm(nt,0,sqrt(tau1))}
+for(i in 1:n[2]){Y2[i,] <- fft_real(Z2sp[i,],inverse=TRUE)+beta.2+rnorm(nt,0,sqrt(tau2))}
 
 ## missing values 
 
 miss1=list()
 miss2=list()
 set.seed(8923)
-for (t in 1:TT)
+for (t in 1:nt)
 {
-  miss1[[t]]=sample(1:n[1],ceiling(TT/5),replace=FALSE)
-  miss2[[t]]=sample(1:n[2],ceiling(TT/5),replace=FALSE)
+  miss1[[t]]=sample(1:n[1],ceiling(nt/5),replace=FALSE)
+  miss2[[t]]=sample(1:n[2],ceiling(nt/5),replace=FALSE)
   Y1[miss1[[t]],t]=NA
   Y2[miss2[[t]],t]=NA
 }
@@ -99,14 +110,14 @@ for (t in 1:TT)
 
 ###### set data frame and plot #######
 
-time=rep(seq(1,TT),each=sum(n))
-type=rep(c(rep('1',n[1]),rep('2',n[2])),TT)
+time=rep(seq(1,nt),each=sum(n))
+type=rep(c(rep('1',n[1]),rep('2',n[2])),nt)
 Y=c(as.vector(Y1),as.vector(Y2))
 U=c(as.vector(u1),as.vector(u2))
-V=c(rep(NA,n[1]*TT),as.vector(v2))
-s1=rep(c(coords1[,1],coords2[,1]),TT)
-s2=rep(c(coords1[,2],coords2[,2]),TT)
-site=rep(seq(1:sum(n)),TT)
+V=c(rep(NA,n[1]*nt),as.vector(v2))
+s1=rep(c(coords1[,1],coords2[,1]),nt)
+s2=rep(c(coords1[,2],coords2[,2]),nt)
+site=rep(seq(1:sum(n)),nt)
 
 
 data=data.frame(time,type,Y,U,V,s1,s2,site)
